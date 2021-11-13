5f47bf2432cfe1be5a911e3f61ff308b7e2167d3
kelleyindahouse <kelleyindahouse@gmail.com>
2021-11-13 17:11:43 -0500

    added prompts for user input

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-// {/* <div class="settings">
-//   <div class="setting">
-//     <label>Password length</label>
-//     <input type="number" id="length" min='4' max='20' value='20' />
-//   </div>
-//   <div class="setting">
-//     <label>Include uppercase letters</label>
-//     <input type="checkbox" id="uppercase" checked />
-//   </div>
-//   <div class="setting">
-//     <label>Include lowercase letters</label>
-//     <input type="checkbox" id="lowercase" checked />
-//   </div>
-//   <div class="setting">
-//     <label>Include numbers</label>
-//     <input type="checkbox" id="numbers" checked />
-//   </div>
-//   <div class="setting">
-//     <label>Include symbols</label>
-//     <input type="checkbox" id="symbols" checked />
-//   </div>
-// </div> */}
-
-// DOM elements
-const result = document.getElementById('result');
-
+// Object to contain declared functions
 const randomFunctions = {
   lower: randomLower,
   upper: randomUpper,
@@ -33,15 +8,15 @@ const randomFunctions = {
 
 // function to generate random lowercase letter
 function randomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26)) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 // function to generate random uppercase letters
 function randomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26)) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 // function to generate random numbers
 function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10)) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 // function to generate random symbols
 function randomSymbol() {
@@ -49,6 +24,24 @@ function randomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+// When generate button is clicked
 document.getElementById('generate').addEventListener('click', () => {
+  // Prompts user for desired password length
+  let passwordLength = prompt("Please enter a number for how long you'd like your password to be. (Min: 8, Max: 128)")
+  passwordLength = parseInt(passwordLength)
+  // Prompts user to include uppercase letters or not
+  let uppercase = prompt("Would you like uppercase letters in your password? (y or n)")
+  // Prompts user to include lowercase letters or not
+  let lowercase = prompt("Would you like lowercase letters in your password? (y or n)")
+  // Prompts user to include numbers or not
+  let numbers = prompt("Would you like numbers in your password? (y or n)")
+  // Prompts user to include symbols or not
+  let symbols = prompt("Would you like symbols in your password? (y or n)")
+
+  for (let i = 8; i <= passwordLength; i++) {
+    
+  }
+
+
 
 })
