c9912a3c06861873d5caec542b4cf976dc0ad7ce
kelleyindahouse <kelleyindahouse@gmail.com>
2021-11-13 20:15:47 -0500

    added validation for user input

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ function randomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+function check() {
+
+}
+
 // When generate button is clicked
 document.getElementById('generate').addEventListener('click', () => {
   // Container for generated password
@@ -23,37 +27,38 @@ document.getElementById('generate').addEventListener('click', () => {
 
   // Prompts user for desired password length
   let userLength = prompt("Please enter a number for how long you'd like your password to be. (Min: 8, Max: 128)");
-
-  if (userLength < 8 || userLength > 128 || userLength === NaN) {
-    alert("Please enter a number that is greater than 7 and less than 129.")
-  }
-
+  
   passLength = parseInt(userLength);
   
+  if (userLength > 7 && userLength < 129) {
+    // Prompts user to include uppercase letters or not
+    let uppercase = prompt("Would you like uppercase letters in your password? (y or n)");
+    // Prompts user to include lowercase letters or not
+    let lowercase = prompt("Would you like lowercase letters in your password? (y or n)");
+    // Prompts user to include numbers or not
+    let numbers = prompt("Would you like numbers in your password? (y or n)");
+    // Prompts user to include symbols or not
+    let symbols = prompt("Would you like symbols in your password? (y or n)");
 
-  // Prompts user to include uppercase letters or not
-  let uppercase = prompt("Would you like uppercase letters in your password? (y or n)");
-  // Prompts user to include lowercase letters or not
-  let lowercase = prompt("Would you like lowercase letters in your password? (y or n)");
-  // Prompts user to include numbers or not
-  let numbers = prompt("Would you like numbers in your password? (y or n)");
-  // Prompts user to include symbols or not
-  let symbols = prompt("Would you like symbols in your password? (y or n)");
-
-  for (let i = 0; i <= passLength; i++) {
-    if (uppercase === 'y') {
-      userPassword += randomUpper()
+    for (let i = 0; i <= passLength; i++) {
+      if (uppercase === 'y') {
+        userPassword += randomUpper()
+      }
+      if (lowercase === 'y') {
+        userPassword += randomLower()
+      }
+      if (numbers === 'y') {
+        userPassword += randomNumber()
+      }
+      if (symbols === 'y') {
+        userPassword += randomSymbol()
+      }
     }
-    if (lowercase === 'y') {
-      userPassword += randomLower()
+  } else { 
+    alert("Please enter a number that is greater than 7 and less than 129.")
+    location.reload();
     }
-    if (numbers === 'y') {
-      userPassword += randomNumber()
-    }
-    if (symbols === 'y') {
-      userPassword += randomSymbol()
-    }
-  }
+    
   // Return generated password set to user submitted password length
   document.getElementById('password').innerHTML = userPassword.substr(0, passLength);
 
